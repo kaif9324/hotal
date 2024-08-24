7469cb40ce4be737881ad1371682df16699c4b5a
kaif9324 <ansarikaif9324@gmail.com>
2024-08-24 18:20:55 +0530

    some changes

--- db.js
+++ db.js
@@ -1,7 +1,11 @@
 const mongoose = require('mongoose')
 // const mongoURl ='mongodb://localhost:27017/al_fajr_hotal'
-const mongoURl = process.env.mongoUrl
-mongoose.connect(mongoURl,{
+// const MONGO_URL = process.env.MONGO_URL;
+// const MONGO_URL='mongodb+srv://akaif595:XAFntEq3L1OfYdSX@cluster0.3tbti.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+const MONGO_URL='mongodb+srv://akaif595:XAFntEq3L1OfYdSX@cluster0.3tbti.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+require('dotenv').config(); // Ensure this is included
+mongoose.connect(MONGO_URL,{
+
     useNewUrlParser:true,
     useUnifiedTopology:true
 })
