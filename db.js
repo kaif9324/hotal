625d2573213042042be0fc386c0a39fc570f1960
kaif9324 <ansarikaif9324@gmail.com>
2024-08-25 12:28:00 +0530

    db and env  update

--- db.js
+++ db.js
@@ -4,11 +4,7 @@ const mongoose = require('mongoose')
 // const MONGO_URL='mongodb+srv://akaif595:XAFntEq3L1OfYdSX@cluster0.3tbti.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
 const MONGO_URL='mongodb+srv://akaif595:XAFntEq3L1OfYdSX@cluster0.3tbti.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
 require('dotenv').config(); // Ensure this is included
-mongoose.connect(MONGO_URL,{
 
-    useNewUrlParser:true,
-    useUnifiedTopology:true
-})
 const db = mongoose.connection;
 db.on('connected',()=>{
     console.log('database connected')
